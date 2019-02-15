6e6e232471c9db865bc38a2492da289fdfb5214b
s5joschi <johannes.schielein@posteo.net>
2019-02-15 14:24:21 +0100

    Added .gitignore file

---
+++ R/test.R
@@ -0,0 +1,14 @@
+#' @title test
+#'
+#' @description This is just a little test for developing packages in R
+#'
+#' @param my_testobject
+#'
+#' @return NULL
+#'
+#' @examples NULL
+#'
+#' @export my_function
+#'
+
+my_function<-function(my_testobject="Javier"){print(paste(my_testobject,"is a litte rat!"))}
