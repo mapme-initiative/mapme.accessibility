5456a980adf3a84b32de85a89d4336d69742b57c
s5joschi <johannes.schielein@posteo.net>
2019-02-22 21:45:31 +0100

    make ras2fricfunction and delte test stuff

---
+++ R/acc_ras2fric.R
@@ -0,0 +1,30 @@
+# function to create a friction input layer from vector data
+#' @title acc_ras2fric
+#'
+#' @description Function to convert vector to friction base data
+#'
+#' @param my_input
+#' @param my_outputname
+#' @param my_baselayer
+#' @param my_speed
+#' @param my_speedfield
+#' @param my_outputpath
+#'
+#' @return r.tmp
+#'
+#' @examples NULL
+#'
+#' @export acc_ras2fric
+#'
+
+# define function
+acc_ras2fric<-
+  function(my_input,
+           my_outputname,
+           my_baselayer,
+           my_speed=NULL,
+           my_speedfield=NULL,
+           my_outputpath=NULL)
+  {
+
+  }
