6c03e2c79776f0adce8f0c89b3b92f7f5ddf0708
s5joschi <johannes.schielein@posteo.net>
2019-02-22 20:38:19 +0100

    Delete test part sinc eit creates a bug

--- R/acc_vec2fric.R
+++ R/acc_vec2fric.R
@@ -95,13 +95,6 @@ acc_vec2fric<-
                                 return(r.tmp)
           }}}}}}}}}}}}
 
-# test it
-r.test<-acc_vec2fric(my_input = test,
-               my_outputname = "test2",
-               my_speedfield = "sup_00",
-               my_baselayer = r.friction,
-               my_outputpath = "output/test/")
-
 
 
 
