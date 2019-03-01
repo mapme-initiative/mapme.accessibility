bf381a0367d4cec8f1542c4160820674334bccc2
s5joschi <johannes.schielein@posteo.net>
2019-03-01 11:55:26 +0100

    enable users to save results

--- R/acc_slopecorr.R
+++ R/acc_slopecorr.R
@@ -14,13 +14,22 @@
 #'
 
 # define function
-acc_slopecorr <- function(my_input, my_radians)
+acc_slopecorr <- function(my_input,
+                          my_radians,
+                          my_output = NULL,
+                          save_results = FALSE)
 {
   if (is.element("raster", installed.packages()[, 1]) == F) {
     print("You do not have 'raster' installed. Please install the package before proceeding")
   } else{
     tmp_stack <- stack(my_input, my_radians)
-    tmp_slopecorr <- tmp_stack[[1]] * (exp(-3 * tan(tmp_stack[[2]])))
+    tmp_slopecorr <-
+      tmp_stack[[1]] * (exp(-3 * tan(tmp_stack[[2]])))
+    ifelse(save_results == TRUE) {
+      writeRaster(x = tmp_slopecorr,
+                  filename = my_output,
+                  datatype = "FLT4S")
+    }
     return(tmp_slopecorr)
   }
 }
