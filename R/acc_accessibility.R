eca261b1217417c8cb9c96e0bf9b3ff3b2ebe6fd
s5joschi <johannes.schielein@posteo.net>
2019-03-01 21:45:41 +0100

    Build function from scratch

--- R/acc_accessibility.R
+++ R/acc_accessibility.R
@@ -1,25 +1,15 @@
-# function to create a friction input layer from vector data
 #' @title acc_accessibility
 #'
-#' @description Function to calculate accessiblity map by
-#' # create a mapregion from friction raster
-#' # Create a new GrassDB, set location and mapset
-#' # Import friction raster to GRASS
-#' # set  grass region from raster
-#' # Import soures - destinites making sure both have same CRS
-#' # calculating accumulated costmap
-#' # exporting cost map as raster
+#' @description Function to calculate accessiblity map
 #'
+#' @param my_friction
+#' @param my_sources
+#' @param knightsmove
+#' @param output_CRS
+#' @param grassbin
+#' @param max_ram
 #'
-#' @param my_input
-#' @param my_outputpath
-#' @param my_outputname
-#' @param my_baselayer
-#' @param my_reclassmatrix
-#' @param my_speedfield
-#' @param my_outputpath
-#'
-#' @return r_tmp
+#' @return tmp_accessibiltiy
 #'
 #' @examples NULL
 #'
@@ -27,3 +17,89 @@
 #'
 
 # define function
+acc_accessibility <-
+  function(my_friction,
+           my_sources,
+           knightsmove = TRUE,
+           grassbin="/usr/lib/grass72/",
+           max_ram=3000) {
+    # check package availability before proceeding
+    if (is.element("raster", installed.packages()[, 1]) == F) {
+      print(
+        "You do not have 'raster' installed. Please install the package before proceeding"
+        )
+    } else{
+      if (is.element("sp", installed.packages()[, 1]) == F) {
+        print(
+          "You do not have 'sp' installed. Please install the package before proceeding"
+        )
+      } else{
+        if (is.element("rgrass7", installed.packages()[, 1]) == F) {
+          print(
+            "You do not have 'rgrass7' installed. Please install the package before proceeding"
+          )
+          else{
+            library("raster")
+            library("sp")
+            library("rgrass7")
+            # (1) Import data
+            print("Setting up grass environment and import data")
+            # Create a new GrassDB, set location and mapset
+            loc <- initGRASS(grassbin,
+                             home=tempdir(),
+                             gisDbase = "grassdb",
+                             location = "grassloc",
+                             mapset = "grassmapset",
+                             override=TRUE)
+            # Import friction raster to GRASS
+            writeRaster(x = my_friction,filename = paste(tempdir(),"/friction.tif"))
+            execGRASS("r.in.gdal",
+                      flags=c("overwrite","o"),
+                      parameters=list(input=paste(tempdir(),"/friction.tif"),
+                                      output="friction"))
+            # set grass region from raster
+            execGRASS("g.region",
+                      parameters=list(raster="friction"))
+            # save input layer and reproject sources crs to match raster crs (if necessary)
+            if(proj4string(my_sources)!=proj4string(my_friction)){
+            writeOGR(obj = spTransform(my_sources,CRSobj = CRS(proj4string(my_friction))),dsn = tempdir(),layer = "sources",driver = "ESRI Shapefile",overwrite_layer = T)
+            }else{writeOGR(obj = my_sources, dsn = tempdir(),layer = "sources",driver = "ESRI Shapefile",overwrite_layer = T)}
+            # import sources
+            execGRASS("v.in.ogr",
+                      flags=c("r","overwrite","o"), # r limits to the current mapregion, o skips projection check -> make sure it is the same was output raster
+                      parameters=list(input=paste(tempdir(),"/sources.shp",sep=""), output="sources"))
+            # calculate accessibility
+            print(paste("Starting to calculate accessiblity map at:"),Sys.time())
+            if (knightsmove==TRUE){
+            execGRASS("r.cost",
+                      flags=c("k","overwrite"), # k forces to use knightsmove, slower but more accurate
+                      parameters=list(input="friction",
+                                      start_points="sources",
+                                      output="accessibility",
+                                      memory=max_ram))
+            }else{
+              execGRASS("r.cost",
+                        flags=c("overwrite"), # no nightsmove
+                        parameters=list(input="friction",
+                                        start_points="sources",
+                                        output="accessibility",
+                                        memory=max_ram))
+            }
+            print(paste("Stopped to calculate accessiblity map at:"),Sys.time())
+            # Export raster as tif
+            execGRASS("r.out.gdal",
+                      flags="overwrite",
+                      parameters=list(input="accessibility",
+                                      output=paste(tempdir(),"accessibility.tif",sep="")
+                      )
+            )
+            tmp_accessibiltiy<-raster(paste(tempdir(),"accessibility.tif",sep=""))
+            return(tmp_accessibiltiy)
+          }
+
+
+
+        }
+      }
+    }
+  }
