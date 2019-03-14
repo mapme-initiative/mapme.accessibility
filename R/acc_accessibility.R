858865bbdc0eef4530c7381791b27aac3430c4f3
s5joschi <johannes.schielein@posteo.net>
2019-03-14 06:45:31 +0100

    improve how to handle temporary files

--- R/acc_accessibility.R
+++ R/acc_accessibility.R
@@ -22,103 +22,102 @@ acc_accessibility <-
            knightsmove = TRUE,
            grassbin,
            max_ram = 3000) {
-          # (1) Import data
-          print("Setting up grass environment and import data")
-          # Create a new GrassDB, set location and mapset
-          loc <- initGRASS(
-            grassbin,
-            home = tempdir(),
-            gisDbase = "grassdb",
-            location = "grassloc",
-            mapset = "grassmapset",
-            override = TRUE
-          )
-          # Import friction raster to GRASS
-          writeRaster(x = my_friction,
-                      filename = paste(tempdir(), "/friction.tif",sep=""))
-          execGRASS(
-            "r.in.gdal",
-            flags = c("overwrite", "o"),
-            parameters = list(
-              input = paste(tempdir(), "/friction.tif",sep=""),
-              output = "friction"
-            )
-          )
-          # set grass region from raster
-          execGRASS("g.region",
-                    parameters = list(raster = "friction"))
-          # save input layer and reproject sources crs to match raster crs (if necessary)
-          if (proj4string(my_sources) != proj4string(my_friction)) {
-            writeOGR(
-              obj = spTransform(my_sources, CRSobj = CRS(proj4string(
-                my_friction
-              ))),
-              dsn = tempdir(),
-              layer = "sources",
-              driver = "ESRI Shapefile",
-              overwrite_layer = T
-            )
-          } else{
-            writeOGR(
-              obj = my_sources,
-              dsn = tempdir(),
-              layer = "sources",
-              driver = "ESRI Shapefile",
-              overwrite_layer = T
-            )
-          }
-          # import sources
-          execGRASS(
-            "v.in.ogr",
-            flags = c("r", "overwrite", "o"),
-            # r limits to the current mapregion, o skips projection check -> make sure it is the same was output raster
-            parameters = list(
-              input = paste(tempdir(), "/sources.shp", sep = ""),
-              output = "sources"
-            )
-          )
-          # calculate accessibility
-          print(paste("Starting to calculate accessiblity map at:",Sys.time()))
-          if (knightsmove == TRUE) {
-            execGRASS(
-              "r.cost",
-              flags = c("k", "overwrite"),
-              # k forces to use knightsmove, slower but more accurate
-              parameters = list(
-                input = "friction",
-                start_points = "sources",
-                output = "accessibility",
-                memory = max_ram
-              )
-            )
-          } else{
-            execGRASS(
-              "r.cost",
-              flags = c("overwrite"),
-              # no nightsmove
-              parameters = list(
-                input = "friction",
-                start_points = "sources",
-                output = "accessibility",
-                memory = max_ram
-              )
-            )
-          }
-          print(paste("Stopped to calculate accessiblity map at:",Sys.time()))
-          # Export raster as tif
-          execGRASS(
-            "r.out.gdal",
-            flags = "overwrite",
-            parameters = list(
-              input = "accessibility",
-              output = paste(tempdir(), "/accessibility.tif", sep =
-                               "")
-            )
-          )
-          tmp_accessibiltiy <-
-            raster(paste(tempdir(), "/accessibility.tif", sep = ""))
-          tmp_accessibiltiy@crs<-sp::CRS(proj4string(my_friction))
-          return(tmp_accessibiltiy)
-          unlink(c(paste(tempdir(), "/*.tif", sep =""),paste(tempdir(), "/sources.*", sep =""),paste(tempdir(), "/grassdb*", sep ="")))
-        }
+    # (1) Import data
+    print("Setting up grass environment and import data")
+    # Create a new GrassDB, set location and mapset
+    filename_1 <- tempfile(pattern = "gisDbase_")
 
+    loc <- initGRASS(
+      grassbin,
+      home = tempdir(),
+      gisDbase = "grassdb",
+      location = "grassloc",
+      mapset = "grassmapset",
+      override = TRUE
+    )
+
+    # create tempname and save friction raster to temp
+    filename_2 <- tempfile(pattern = "raster_", fileext = ".tif")
+    writeRaster(x = my_friction,
+                filename = filename_2)
+    # Import friction raster to GRASS
+    execGRASS(
+      "r.in.gdal",
+      flags = c("overwrite", "o"),
+      parameters = list(input = filename_2,
+                        output = "friction")
+    )
+    # set grass region from raster
+    execGRASS("g.region",
+              parameters = list(raster = "friction"))
+    # save input layer and reproject sources crs to match raster crs (if necessary)
+    filename_3 <-
+      gsub("/", "", tempfile(pattern = "tempvector", tmpdir = ""))
+    if (proj4string(my_sources) != proj4string(my_friction)) {
+      writeOGR(
+        obj = spTransform(my_sources, CRSobj = CRS(proj4string(my_friction))),
+        dsn = tempdir(),
+        layer = filename_3,
+        driver = "ESRI Shapefile",
+        overwrite_layer = T
+      )
+    } else{
+      writeOGR(
+        obj = my_sources,
+        dsn = tempdir(),
+        layer = filename_3,
+        driver = "ESRI Shapefile",
+        overwrite_layer = T
+      )
+    }
+    # import sources
+    execGRASS(
+      "v.in.ogr",
+      flags = c("r", "overwrite", "o"),
+      # r limits to the current mapregion, o skips projection check -> make sure it is the same was output raster
+      parameters = list(
+        input = paste(filename_3, ".shp", sep = ""),
+        output = "sources"
+      )
+    )
+    # calculate accessibility
+    print(paste("Starting to calculate accessiblity map at:", Sys.time()))
+    if (knightsmove == TRUE) {
+      execGRASS(
+        "r.cost",
+        flags = c("k", "overwrite"),
+        # k forces to use knightsmove, slower but more accurate
+        parameters = list(
+          input = "friction",
+          start_points = "sources",
+          output = "accessibility",
+          memory = max_ram
+        )
+      )
+    } else{
+      execGRASS(
+        "r.cost",
+        flags = c("overwrite"),
+        # no nightsmove
+        parameters = list(
+          input = "friction",
+          start_points = "sources",
+          output = "accessibility",
+          memory = max_ram
+        )
+      )
+    }
+    print(paste("Stopped to calculate accessiblity map at:", Sys.time()))
+    # Export raster as tif
+    filename_4 <- tempfile(pattern = "raster_", fileext = ".tif")
+    execGRASS(
+      "r.out.gdal",
+      flags = "overwrite",
+      parameters = list(input = "accessibility",
+                        output = filename_4)
+    )
+    tmp_accessibiltiy <-
+      raster(filename_4)
+    tmp_accessibiltiy@crs <- sp::CRS(proj4string(my_friction))
+    return(tmp_accessibiltiy)
+  }
